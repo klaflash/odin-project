9079ddcd8ba7d00eb6bce8992682209799c5f01e
Kyle <klaflash@purdue.edu>
2022-05-30 22:14:12 -0400

    Created odin-rps file containing index.html and game.js

---
+++ odin-rps/game.js
@@ -0,0 +1,14 @@
+//randomly return Rock, Paper, or Scissors
+function computerPlay() {
+    
+}
+
+//return the winner in the form of a string
+function playRound(playerSelection, computerSelection) {
+
+}
+
+//play a 5 round game
+function game() {
+
+}
