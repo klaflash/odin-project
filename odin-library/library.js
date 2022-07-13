a7cc86bcbb8d0264977d3a1db468968b46d6816b
Kyle <klaflash@purdue.edu>
2022-07-13 17:50:26 -0400

    Implemented popup modal to add a new book

--- odin-library/library.js
+++ odin-library/library.js
@@ -21,24 +21,25 @@ Book.prototype.info = function() {
 
 }
 
-let book_title = window.prompt("Title: ");
-let book_author = window.prompt("Author: ");
-let book_pages = window.prompt("Pages: ");
-let book_read = window.prompt("Read? ");
-let book_rating = window.prompt("Rating: ");
+//let book_title = window.prompt("Title: ");
+//let book_author = window.prompt("Author: ");
+//let book_pages = window.prompt("Pages: ");
+//let book_read = window.prompt("Read? ");
+//let book_rating = window.prompt("Rating: ");
 
-function addBookToLibrary() {
+function addBookToLibrary(book_title, book_author, book_pages, book_read, book_rating) {
 
     const temp = new Book(book_title, book_author, book_pages, book_read, book_rating);
     myLibrary.push(temp);
-
+    display();
 }
 
-addBookToLibrary();
+//addBookToLibrary();
 
 function display() {
 
-    for (const book of myLibrary) {
+    const book = myLibrary[myLibrary.length - 1];
+
         const tag = document.createElement('div');
         const text = document.createTextNode(book.info());
         tag.appendChild(text);
@@ -47,7 +48,28 @@ function display() {
 
         var element = document.querySelector('.container');
         element.appendChild(tag);
-    }
 }
 
 display();
+
+document.getElementById('new-book').addEventListener('click', function() {
+
+    document.querySelector('.popup').style.display = 'flex';
+
+});
+
+document.getElementById('close-popup').addEventListener('click', function() {
+    document.querySelector('.popup').style.display = 'none';
+});
+
+document.getElementById('submit-button').addEventListener('click', function() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementsByName('read').value;
+    const rating = document.getElementById('rating').value;
+
+    addBookToLibrary(title, author, pages, read, rating);
+
+    document.querySelector('.popup').style.display = 'none';
+});
