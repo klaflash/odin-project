124bb6d673ff57b7ed819f96526667148588ef3c
Kyle <klaflash@purdue.edu>
2022-07-13 01:02:03 -0400

    Implemented basic functionality of adding a book to the library

--- odin-library/library.js
+++ odin-library/library.js
@@ -18,6 +18,30 @@ Book.prototype.info = function() {
 
 }
 
+let book_title = window.prompt("Title: ");
+let book_author = window.prompt("Author: ");
+let book_pages = window.prompt("Pages: ");
+let book_read = window.prompt("Read? ");
+let book_rating = window.prompt("Rating: ");
+
 function addBookToLibrary() {
 
+    const temp = new Book(book_title, book_author, book_pages, book_read, book_rating);
+    myLibrary.push(temp);
+
 }
+
+addBookToLibrary();
+
+function display() {
+
+    for (const book of myLibrary) {
+        const tag = document.createElement('div');
+        const text = document.createTextNode(book.info());
+        tag.appendChild(text);
+        var element = document.querySelector('.container');
+        element.appendChild(tag);
+    }
+}
+
+display();
